dcabbddfd181064cfdd08cb04fbb70274f719bf4
evxeen <evxeen@gmail.com>
2022-05-12 22:34:00 +0300

    feat: pagination(router)

--- src/components/SearchField/SearchField.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import s from "./SearchField.module.scss";
-
-export const SearchField = () => {
-  return <input className={s.searchField} type="text" placeholder="Поиск" />;
-};
